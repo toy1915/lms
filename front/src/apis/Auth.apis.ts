7b7b80e0c4b5f93b7fd1a745daaba9bb6234da95
byoungyoon <bur5698@naver.com>
2022-04-06 09:54:36 +0900

    next로 구조 변경으로 초기화

--- front/src/apis/Auth.apis.ts
+++
@@ -1,8 +0,0 @@
-import { LoginRequest, LoginResponse } from '@type/Login.type';
-import { customAxios } from '@apis/Axios-module';
-import { TokenRequest } from '@type/Auth.type';
-
-export const authApis = {
-  login: async (loginRequest: LoginRequest) => await customAxios.post<LoginResponse>('/api/login', loginRequest),
-  reissue: async (tokenRequest: TokenRequest) => await customAxios.post<LoginResponse>('/api/reissue', tokenRequest),
-};
--- front/src/constants/Token.ts
+++
@@ -1,5 +0,0 @@
-export const TOKEN = {
-  ACCESS_TOKEN: 'accessToken',
-  REFRESH_TOKEN: 'refreshToken',
-  ACCESS_TOKEN_EXP: 'accessTokenExpiresIn',
-};
